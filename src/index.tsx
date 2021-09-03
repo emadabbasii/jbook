b6b009a757b55389dc535763f254a2fe0b627931
emad abbasii <emadabbasii137@gmail.com>
2021-09-03 18:23:59 +0430

    adding a tiny test package

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,55 @@
 import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
-import {useState , useEffect , useRef} from "react";
-
+import { useState, useEffect, useRef } from "react";
+import { unpkPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-  const ref = useRef<any>()
+  const ref = useRef<any>();
 
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm"
+      wasmURL: "/esbuild.wasm",
     });
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     startService();
-  },[])
+  }, []);
 
-  const onClick = () => {
-    if(!ref.current){
-      return ;
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
-    console.log(ref.current);
-  }
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkPathPlugin()],
+    });
+
+    // console.log(result);
+    setCode(result.outputFiles[0].text);
+    // const result = await ref.current.transform(input, {
+    //   loader: "jsx",
+    //   target: "es2015",
+    // });
+
+    // setCode(result.code);
+    // console.log(result);
+  };
   return (
     <div>
-      <textarea value={input} onChange={e => setInput(e.target.value)}>
-
-      </textarea>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
   );
-}
-ReactDOM.render(<App/>, document.querySelector('#root'));
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
